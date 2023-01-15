469a1caa11768b6dcf05fbd5b982cee1b76dcd31
Hapis <96516537+HapisPro@users.noreply.github.com>
2023-01-15 20:51:58 +0700

    Delete 05-Model directory

--- 05-Model/app/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Untuk 'link' semua file
-require 'config/config.php';
-
-spl_autoload_register( function ( $class ) {
-    require __DIR__ . '/core/' . $class . '.php';
-} );
--- 05-Model/public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Bootstraping = memanggil seluruh file mvc
-require '../app/init.php';
-
-$app = new App;
